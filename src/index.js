5ca9c370212cbdf46c37a81dd66d989b88e1550e
Yuliya <vsggdfaf@gmail.com>
2022-02-12 12:04:52 +0300

    добавление модуля slider_services

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import modal_menu from "./modules/modal_menu";
 import modal from "./modules/modal";
+import modal from "./modules/slider_services";
 
 timer('15 february 2022')
 modal_menu()
 modal()
+slider_services()
