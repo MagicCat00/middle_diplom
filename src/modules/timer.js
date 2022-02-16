c5cbfdf675a2f888fa36e98faffba21c5a230550
Yuliya <vsggdfaf@gmail.com>
2022-02-16 11:02:05 +0300

    добавление второго таймера

--- src/modules/timer.js
+++ src/modules/timer.js
@@ -1,9 +1,6 @@
 const timer = (deadline) => {
-    const timerDays = document.getElementById('timer-days');
-    const timerHours = document.getElementById('timer-hours');
-    const timerMinutes = document.getElementById('timer-minutes');
-    const timerSeconds = document.getElementById('timer-seconds');
- 
+    const timerBlocks = document.querySelectorAll('.count-wrap')
+
     const getTimeRemaining = () => {
         let dateStop = new Date(deadline).getTime()
         let dateNow = new Date().getTime()
@@ -12,27 +9,31 @@ const timer = (deadline) => {
         let hours = Math.floor((timeRemaining / 60 / 60) % 24)
         let minutes = Math.floor((timeRemaining / 60) % 60)
         let seconds = Math.floor(timeRemaining % 60)
-
-
-        return { timeRemaining, days, hours, minutes, seconds }
+        return {timeRemaining, days, hours, minutes, seconds}
     }
-    //countTimer('22 february 2022')
 
     const updateClock = () => {
         let getTime = getTimeRemaining()
-        
-        timerDays.textContent = getTime.days < 10 ? '0' + getTime.days : getTime.days;
-        timerHours.textContent = getTime.hours < 10 ? '0' + getTime.hours : getTime.hours;
-        timerMinutes.textContent = getTime.minutes < 10 ? '0' + getTime.minutes : getTime.minutes;
-        timerSeconds.textContent = getTime.seconds < 10 ? '0' + getTime.seconds : getTime.seconds;
+        timerBlocks.forEach(timerBlock => {
+            const timerDays = timerBlock.querySelector('#timer-days')
+            const timerHours = timerBlock.querySelector('#timer-hours')
+            const timerMinutes = timerBlock.querySelector('#timer-minutes')
+            const timerSeconds = timerBlock.querySelector('#timer-seconds') 
 
-        if (getTime.timeRemaining > 0) {
-            setTimeout(updateClock, 1000)
-        }
-        
+            timerDays.textContent = getTime.days < 10 ? '0' + getTime.days : getTime.days;
+            timerHours.textContent = getTime.hours < 10 ? '0' + getTime.hours : getTime.hours;
+            timerMinutes.textContent = getTime.minutes < 10 ? '0' + getTime.minutes : getTime.minutes;
+            timerSeconds.textContent = getTime.seconds < 10 ? '0' + getTime.seconds : getTime.seconds;
+
+            if (getTime.timeRemaining > 0) {
+                setTimeout(updateClock, 1000)
+            }
+        })
+
     }
+
+
     updateClock()
-    
 }
 
 export default timer
